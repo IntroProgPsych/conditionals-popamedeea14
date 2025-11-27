faffabb6d90fb5730c520db6bac934509f77d656
popamedeea14 <maria.popa06@e-uvt.ro>
2025-11-27 10:47:33 +0000

    committed

--- Part2_Copilot/7_Greater.py
+++ Part2_Copilot/7_Greater.py
@@ -20,3 +20,12 @@
 # The numbers are equal!
 
 # Write your code here:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+else:
+    print("The numbers are equal.")
