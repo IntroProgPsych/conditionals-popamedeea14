b64a90aba88b11664f737d79d984933e7fd21d4c
popamedeea14 <maria.popa06@e-uvt.ro>
2025-11-27 10:35:26 +0000

    committed

--- Part1/3_Order.py
+++ Part1/3_Order.py
@@ -25,4 +25,12 @@
 # Thank you!
 
 # Write your code here:
+number = int(input("Please type in a number: "))
+if number < 1000:
+    print("This number is smaller than 1000")
+if number < 100:
+    print("This number is smaller than 100")
+if number < 10:
+    print("This number is smaller than 10")
+print("Thank you!")
 
