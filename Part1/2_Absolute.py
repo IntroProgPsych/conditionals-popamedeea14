bd7fd1365f28328a04ba0caa768b988f3dcafbb6
popamedeea14 <maria.popa06@e-uvt.ro>
2025-11-27 10:01:50 +0000

    committed

--- Part1/2_Absolute.py
+++ Part1/2_Absolute.py
@@ -16,4 +16,9 @@
 # The absolute value of this number is 99
 
 # Write your code here:
+number = int(input("Please type in a number: "))
+if number < 0:
+    print("The absolute value of this number is", number * -1)
+else:
+    print("The absolute value of this number is", number)
 
